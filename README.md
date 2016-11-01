8c7a7ebd482b3871d1b774564e02d17c838c1ebb
John Collins <jmicahc@gmail.com>
2016-11-01 16:58:05 -0700

    update readme

--- README.md
+++ README.md
@@ -4,15 +4,18 @@ This repository includes several algorithms for generating prime numbers, incudi
 
 ## How to Run
 - Clone this repository
-- at the root, run `lein compile`, then `lein uberjar`.
-- then, `java -jar target/funding_circle-standalone.jar`. You should see a table printed of the cartessian product of the first 10 primes.
+- Make sure leiningen version `2.7.1` or greater is installed (any version > 2.0 will probably work too).
+- At the root, run `lein compile`, then `lein uberjar`
+- Then, `target/funding_circle-0.1.0-SNAPSHOT-standalone.jar`. You should see a table printed of the cartessian product of the first 10 primes
 
 ## Dependencies
+- Clojure version 1.7+
+- Leiningen version 2.0+
 - clojure.data.priority-map. Leiningen will download this automatically when compiling the project.
 
 ## Notes
-- See test/funding_circle simple tests of each function.
-- See test/funding_circle/performance_tests for empirical performance tests.
+- See test/funding_circle for simple tests of each function
+- See test/funding_circle/performance_tests for empirical performance tests (summary below)
 
 ## Performane
 Time to Calculate the first N primes (ms)
@@ -20,6 +23,8 @@ Time to Calculate the first N primes (ms)
 | # Primes  | brute-force | eager TD | optimized TD |  Segmented Sieve | wheel2357 Sieve |
 | ----------| -----------:| --------:| ------------:|-----------------:|----------------:|
 |   10      |   0.052281  | 2.018853 |   0.00433    |     0.006848     |    0.006669     |
-|   100     |   0.045831  | 2.018853 |   0.00441    |     2.700833     |    0.003956     | 
+|   100     |   11.64237  | 10.70934 |   6.11690    |     3.638701     |    3.219721     | 
 |   1000    |   1065.666  | 292.2991 |   33.0679    |     55.74185     |    25.25332     |
 |   10000   |   121592.5  | 22592.33 |   719.024    |     1101.805     |    552.2367     |
+
+These results are consistent with theoretical predictions. See complexity_analysis.md for more details.
--- test/funding_circle/test_performance.clj
+++ test/funding_circle/test_performance.clj
@@ -1,23 +1,30 @@
 (ns funding_circle.test-performance
   (:require [funding_circle.prime-algorithms :as algs]))
 
-defn test-perf [n]
-(time (vec (take n algs/brute-force)))
-(time (vec (algs/eager-trial-division n)))
-(if (<= n 1000)
-  (time (vec (take n algs/lazy-trial-division)))
-  (println "n/a"))
-#_(time (vec (take n) algs/lazy-trial-division-xf))
-(time (vec (take n algs/optimized-trial-division)))
-(time (vec (algs/segmented-sieve 547)))
+(defn test-perf [n]
+  (time (vec (take n algs/brute-force)))
+  (time (vec (algs/eager-trial-division n)))
+  (if (<= n 1000)
+    (time (vec (take n algs/lazy-trial-division)))
+    (println "n/a"))
+  (time (vec (take n algs/lazy-trial-division-xf)))
+  (time (vec (take n algs/optimized-trial-division)))
+  (time (vec (algs/segmented-sieve 51)))
+  (time (vec (take n algs/wheel2-primes)))
+  (time (vec (take n algs/wheel23-primes)))
+  (time (vec (take n algs/wheel235-primes)))
+  (time (vec (take n algs/wheel2357-primes)))
+  nil)
 
-(nth algs/wheel2357-primes 100)
-;; Algorithm | brute-force | eager TD | optimized TD |  Segmented Sieve | wheel2357 Sieve | # primes
-;; time (ms) |   0.052281  | 2.018853 |   0.00433    |     0.006848     |    0.006669     | 10
-;;           |   0.045831  | 2.018853 |   0.00441    |     2.700833     |    0.003956     | 100
-;;           |   1065.666  | 292.2991 |   33.0679    |     55.74185     |    25.25332     | 1000
-;;           |   121592.5  | 22592.33 |   719.024    |     1101.805     |    552.2367     | 10000   
+;; Time to Calculate the first N primes (ms)
 
+;; # Primes  | brute-force | eager TD | optimized TD |  Segmented Sieve | wheel2357 Sieve |
+;; ----------|:----------- | --------:| ------------:|-----------------:|----------------:|
+;;   10      |   0.052281  | 2.018853 |   0.00433    |     0.006848     |    0.006669     |
+;;   100     |   0.045831  | 2.018853 |   0.00441    |     2.700833     |    0.003956     | 
+;;   1000    |   1065.666  | 292.2991 |   33.0679    |     55.74185     |    25.25332     |
+;;   10000   |   121592.5  | 22592.33 |   719.024    |     1101.805     |    552.2367     |
+(neth (take n algs/wheel2357-primes))
 (comment 
   n = 10
   "Elapsed time: 0.052281 msecs"
@@ -31,18 +38,19 @@ defn test-perf [n]
   "Elapsed time: 0.009309 msecs"
   "Elapsed time: 0.006669 msecs")
 
+(test-perf 100)
 (comment
   n = 100
-  "Elapsed time: 0.045831 msecs"
-  "Elapsed time: 11.465948 msecs"
-  "Elapsed time: 0.010282 msecs"
-  "Elapsed time: 0.004413 mecs"
-  "Elapsed time: 0.003689 msecs"
-  "Elapsed time: 10.622899 msecs"
-  "Elapsed time: 0.007634 msecs"
-  "Elapsed time: 0.008717 msecs"
-  "Elapsed time: 0.004159 msecs"
-  "Elapsed time: 0.003956 msecs")
+  "Elapsed time: 11.642368 msecs"
+  "Elapsed time: 10.709341 msecs"
+  "Elapsed time: 72.783503 msecs"
+  "Elapsed time: 80.616424 msecs"
+  "Elapsed time: 6.116903 msecs"
+  "Elapsed time: 3.638701 msecs"
+  "Elapsed time: 21.179786 msecs"
+  "Elapsed time: 5.905872 msecs"
+  "Elapsed time: 17.319495 msecs"
+  "Elapsed time: 3.219721 msecs")
 
 (comment
   n = 1000
